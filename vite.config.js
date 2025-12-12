77a77e6c643b0b9cde5ac068e5bf1b93129bc3ef
SoyVixterDev <soyvixterdev@gmail.com>
2025-12-12 20:09:43 +0800

    Updated vite config again

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://soyvixterdev.github.io/personal-site/",
+  base: "/personal-site/dist",
   plugins: [react()],
 })
